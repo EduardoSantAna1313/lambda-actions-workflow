bb51057bac1cc948d350ef1cc9fd1814f873bd12
Eduardo <eduardosantana1313@gmail.com>
2023-10-13 21:23:37 -0300

    Update function

--- app/index.js
+++ app/index.js
@@ -1,3 +1,20 @@
-export const handler = async(event) => {
-    return "<html><h1>HELLO WORLD</h1></html>"
+const html =  '<html>'
+    + '<head><title>Gerar imagem</title></head>' 
+    + '<body>'
+    + '<h1>Generate image from prompt</h1>'
+    + '<p>Prompt</p><input type="text"></input>'
+    + '<button>OK</button>'
+    + '</body>'
+    + '</html>';
+
+export const handler = function(event, context, callback) {
+    var res ={
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html"
+        }
+    };
+    
+    res.body = html;
+    callback(null, res);
 };
